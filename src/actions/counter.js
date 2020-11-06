a60473cea192b13daef9c8c680e96de9f988b9fb
Matheus Costa <matheus271200.ms@gmail.com>
2020-11-05 22:36:32 -0300

    Initial commit

---
+++ src/actions/counter.js
@@ -0,0 +1,12 @@
+import { actionsTypes } from "../constants/counter";
+
+const actions = {
+  increment: () => ({
+    type: actionsTypes.COUNTER_INCREMENT,
+  }),
+  decrement: () => ({
+    type: actionsTypes.COUNTER_DECREMENT,
+  }),
+};
+
+export { actions };
---
+++ src/containers/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Counter from '../components/Counter'
+
+const App = () => <Counter></Counter>
+
+export default App;
